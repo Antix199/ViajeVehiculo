b77ee5be2c36c3aeed5e332180e2b7254973814d
Antix199 <142464955+Antix199@users.noreply.github.com>
2023-10-15 21:52:43 -0300

    Add files via upload

--- test/java/ValidacionesTest.java
+++ test/java/ValidacionesTest.java
@@ -1,14 +1,38 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-class ValidacionesTest {
-
+public class ValidacionesTest {
     @Test
-    void validarEnteroPositivo() {
+    public void testValidarEnteroPositivo() {
+        String input = "5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        int resultado = Validaciones.validarEnteroPositivo("Ingresa un número entero positivo: ");
+
+        assertEquals(5, resultado);
     }
 
     @Test
-    void validarRango() {
+    public void testValidarEnteroPositivoConValorNegativo() {
+        String input = "-3\n5";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        int resultado = Validaciones.validarEnteroPositivo("Ingresa un número entero positivo: ");
+
+        assertEquals(5, resultado);
     }
+    public void testValidarRango() {
+        String input = "2";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        int resultado = Validaciones.validarRango("Elija una opción:", 1, 3);
+
+        assertEquals(2, resultado);
+    }
+
 }
--- test/java/VehiculoTest.java
+++ test/java/VehiculoTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class VehiculoTest {
     @Test
-    public void testCalcularTiempoDeViaje() {
+    public void TestCalcularTiempoDeViaje() {
         Vehiculo auto = new Vehiculo("auto", 30.0);
         double distancia = 100.0;
         double tiempoEsperado = 3.33;
